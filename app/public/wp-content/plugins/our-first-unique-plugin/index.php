2fa54b7246622acefcb30cc6c18963c08c67401c
Fabio Pontes <faabiopontes@gmail.com>
2022-06-04 10:58:36 -0300

    S23L120: Finishing Our Settings Form - Refactored all the array() sintax to be [] instead

--- app/public/wp-content/plugins/our-first-unique-plugin/index.php
+++ app/public/wp-content/plugins/our-first-unique-plugin/index.php
@@ -14,26 +14,55 @@ class WordCountAndTimePlugin
     {
         $this->menu_slug = 'word-count-settings-page';
         $this->option_group = 'wordcountplugin';
-        add_action('admin_menu', array($this, 'adminPage'));
-        add_action('admin_init', array($this, 'settings'));
+        add_action('admin_menu', [$this, 'adminPage']);
+        add_action('admin_init', [$this, 'settings']);
     }
 
     function settings() {
         add_settings_section('wcp_first_section', null, null, 'word-count-settings-page');
-        add_settings_field('wcp_location', 'Display Location', array($this, 'locationHTML'), $this->menu_slug, 'wcp_first_section');
-        register_setting($this->option_group, 'wcp_location', array('sanitize_callback' => 'sanitize_text_field', 'default' => '0'));
+
+        add_settings_field('wcp_location', 'Display Location', [$this, 'locationHTML'], $this->menu_slug, 'wcp_first_section');
+        register_setting($this->option_group, 'wcp_location', ['sanitize_callback' => [$this, 'sanitizeLocation'], 'default' => '0']);
+
+        add_settings_field('wcp_headline', 'Headline Text', [$this, 'headlineHTML'], $this->menu_slug, 'wcp_first_section');
+        register_setting($this->option_group, 'wcp_headline', ['sanitize_callback' => 'sanitize_text_field', 'default' => 'Post Statistics']);
+
+        add_settings_field('wcp_wordcount', 'Word Count', [$this, 'checkboxHTML'], $this->menu_slug, 'wcp_first_section', ['fieldName' => 'wcp_wordcount']);
+        register_setting($this->option_group, 'wcp_wordcount', ['sanitize_callback' => 'sanitize_text_field', 'default' => '1']);
+
+        add_settings_field('wcp_charactercount', 'Character Count', [$this, 'checkboxHTML'], $this->menu_slug, 'wcp_first_section', ['fieldName' => 'wcp_charactercount']);
+        register_setting($this->option_group, 'wcp_charactercount', ['sanitize_callback' => 'sanitize_text_field', 'default' => '1']);
+
+        add_settings_field('wcp_readtime', 'Read Time', [$this, 'checkboxHTML'], $this->menu_slug, 'wcp_first_section', ['fieldName' => 'wcp_readtime']);
+        register_setting($this->option_group, 'wcp_readtime', ['sanitize_callback' => 'sanitize_text_field', 'default' => '1']);
+    }
+
+    function sanitizeLocation($input) {
+        if ($input != '0' AND $input != '1') {
+            add_settings_error('wcp_location', 'wcp_location_error', 'Invalid Display Location value');
+            return get_option('wcp_location');
+        }
+        return $input;
     }
 
     function locationHTML() { ?>
         <select name="wcp_location">
-            <option value="0">Beginning of post</option>
-            <option value="1">End of post</option>
+            <option value="0" <?php selected(get_option('wcp_location'), '0') ?>>Beginning of post</option>
+            <option value="1" <?php selected(get_option('wcp_location'), '1') ?>>End of post</option>
         </select>
+    <?php }
+
+    function checkboxHTML($args) { ?>
+        <input type="checkbox" name="<?php echo $args['fieldName'] ?>" value="1" <?php checked(get_option($args['fieldName']), '1') ?>>
+    <?php }
+
+    function headlineHTML() { ?>
+        <input type="text" name="wcp_headline" value="<?php echo esc_attr(get_option('wcp_headline')) ?>">
     <?php }
 
     function adminPage($adminMenu)
     {
-        add_options_page('Word Count Settings', 'Word Count', 'manage_options', $this->menu_slug, array($this, 'ourHTML'));
+        add_options_page('Word Count Settings', 'Word Count', 'manage_options', $this->menu_slug, [$this, 'ourHTML']);
     }
 
     function ourHTML()
