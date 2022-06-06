8dfe7fdd16b524d252db080818680a3cb871b13f
Fabio Pontes <faabiopontes@gmail.com>
2022-06-06 20:51:08 -0300

    S23L124: Custom Admin Menu Icon - Added custom SVG as a Base64-encoded ASCII string (using JS btoa) - Added custom CSS file with our own CSS

--- app/public/wp-content/plugins/our-word-filter-plugins/index.php
+++ app/public/wp-content/plugins/our-word-filter-plugins/index.php
@@ -17,13 +17,27 @@ class OurWordFilterPlugin {
     }
 
     function ourMenu() {
-        add_menu_page('Words To Filter', 'Word Filter', 'manage_options', $this->menu_slug, [$this, 'wordFilterPage'], 'dashicons-smiley', 100);
+        $mainPageHook = add_menu_page('Words To Filter', 'Word Filter', 'manage_options', $this->menu_slug, [$this, 'wordFilterPage'], 'data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjAiIGhlaWdodD0iMjAiIHZpZXdCb3g9IjAgMCAyMCAyMCIgZmlsbD0ibm9uZSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KPHBhdGggZmlsbC1ydWxlPSJldmVub2RkIiBjbGlwLXJ1bGU9ImV2ZW5vZGQiIGQ9Ik0xMCAyMEMxNS41MjI5IDIwIDIwIDE1LjUyMjkgMjAgMTBDMjAgNC40NzcxNCAxNS41MjI5IDAgMTAgMEM0LjQ3NzE0IDAgMCA0LjQ3NzE0IDAgMTBDMCAxNS41MjI5IDQuNDc3MTQgMjAgMTAgMjBaTTExLjk5IDcuNDQ2NjZMMTAuMDc4MSAxLjU2MjVMOC4xNjYyNiA3LjQ0NjY2SDEuOTc5MjhMNi45ODQ2NSAxMS4wODMzTDUuMDcyNzUgMTYuOTY3NEwxMC4wNzgxIDEzLjMzMDhMMTUuMDgzNSAxNi45Njc0TDEzLjE3MTYgMTEuMDgzM0wxOC4xNzcgNy40NDY2NkgxMS45OVoiIGZpbGw9IiNGRkRGOEQiLz4KPC9zdmc+Cg==', 100);
         add_submenu_page($this->menu_slug, 'Words To Filter', 'Word List', 'manage_options', $this->menu_slug, [$this, 'wordFilterPage']);
         add_submenu_page($this->menu_slug, 'Word Filter Options', 'Options', 'manage_options', 'word-filter-options', [$this, 'optionsSubPage']);
+        add_action("load-{$mainPageHook}", [$this, 'mainPageAssets']);
+    }
+
+    function mainPageAssets() {
+        wp_enqueue_style('filterAdminCss', plugin_dir_url(__FILE__) . 'styles.css');
     }
 
     function wordFilterPage() { ?>
-        Hello World.
+        <div class="wrap">
+            <h1>Word Filter</h1>
+            <form method="post">
+                <label for="plugin_words_to_filter"><p>Enter a <strong>comma-separated</strong> list of words to filter from your site's content</p></label>
+                <div class="word-filter__flex-container">
+                    <textarea name="plugin_words_to_filter" id="plugin_words_to_filter" placeholder="bad, mean, awful, horrible"></textarea>
+                </div>
+                <input type="submit" name="submit" id="submit" class="button button-primary" value="Save Changes">
+            </form>
+        </div>
     <?php }
 
     function optionsSubPage() { ?>
