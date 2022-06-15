b89f09218d9c8b105d7e8a0fcf3f2ef2d78b34d4
Fabio Pontes <faabiopontes@gmail.com>
2022-06-14 21:23:01 -0300

    S24L129: Block Type Attributes - Added attributes in our plugin block

--- app/public/wp-content/plugins/are-you-paying-attention/src/index.js
+++ app/public/wp-content/plugins/are-you-paying-attention/src/index.js
@@ -2,22 +2,29 @@ wp.blocks.registerBlockType("ourplugin/are-you-paying-attention", {
     title: "Are You Paying Attention?",
     icon: "smiley",
     category: "common",
-    edit: function () {
+    attributes: {
+        skyColor: { type: "string" },
+        grassColor: { type: "string" }
+    },
+    edit: function (props) {
         // This return is what is shown on admin when page is being editted
+        const updateSkyColor = (event) => {
+            props.setAttributes({ skyColor: event.target.value });
+        }
+        const updateGrassColor = (event) => {
+            props.setAttributes({ grassColor: event.target.value });
+        }
         return (
             <div>
-                <p>Hello, this is a paragraph.</p>
+                <input type="text" placeholder="sky color" value={props.attributes.skyColor} onChange={updateSkyColor} />
+                <input type="text" placeholder="grass color" value={props.attributes.grassColor} onChange={updateGrassColor} />
             </div>
         )
-        //wp.element.createElement("h3", null, "Hello, this is from the admin editor screen.");
     },
-    save: function () { 
+    save: function (props) {
         // This return is what is saved on the database and shown when the post is open
         return (
-            <>
-                <h3>H3 on the frontend.</h3>
-                <h5>H5 on the frontend.</h5>
-            </>
+            <p>Today the sky is {props.attributes.skyColor} and the grass is {props.attributes.grassColor}</p>
         )
     }
 });
