d18a3bdd94d44b866f298d5c38e599e1ffd84f2a
Fabio Pontes <faabiopontes@gmail.com>
2022-08-21 19:06:21 -0300

    S3L8: PHP Functions

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,1 +1,11 @@
-Thsi is our amazing custom theme.
+<?php
+function greet($name, $color)
+{
+    echo "<p>Hi, my name is $name and my favorite color is $color.</p>";
+}
+greet('John', 'blue');
+greet('Jane', 'red');
+?>
+
+<h1><?php bloginfo('name'); ?></h1>
+<p><?php bloginfo('description'); ?></p>
