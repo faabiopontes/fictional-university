40bf6c214bc0f0f039c6aad1cadfb81aace36b4c
Fabio Pontes <faabiopontes@gmail.com>
2022-08-25 19:58:04 -0300

    S4L10: The Famous "Loop" in WordPress

--- app/public/wp-content/themes/fictional-university-theme/index.php
+++ app/public/wp-content/themes/fictional-university-theme/index.php
@@ -1,11 +1,9 @@
 <?php
-function greet($name, $color)
-{
-    echo "<p>Hi, my name is $name and my favorite color is $color.</p>";
-}
-greet('John', 'blue');
-greet('Jane', 'red');
+    while(have_posts()) {
+        the_post(); // each time the_post runs it gets info from the next post
 ?>
+<h2><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
+<?php the_content(); ?>
+<hr/>
 
-<h1><?php bloginfo('name'); ?></h1>
-<p><?php bloginfo('description'); ?></p>
+<?php } ?>
---
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -0,0 +1,5 @@
+<?php
+the_post(); // each time the_post runs it gets info from the next post
+?>
+<h2><?php the_title(); ?></h2>
+<?php the_content(); ?>
