c61212ebde4c1782f886b8b185d4de27e7d6f869
Fabio Pontes <faabiopontes@gmail.com>
2022-08-31 20:57:39 -0300

    S5L15: Interior Page Template

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -1,9 +1,39 @@
 <?php
     get_header();
-    the_post(); // each time the_post runs it gets info from the next post
+
+    while(have_posts()):
+        the_post();
 ?>
-<h2><?php the_title(); ?></h2>
+<div class="page-banner">
+      <div class="page-banner__bg-image" style="background-image: url(<?=get_theme_file_uri('/images/ocean.jpg')?>)"></div>
+      <div class="page-banner__content container container--narrow">
+        <h1 class="page-banner__title"><?=the_title(); ?></h1>
+        <div class="page-banner__intro">
+          <p>DON'T FORGET TO REPLACE ME LATER</p>
+        </div>
+      </div>
+    </div>
+
+    <div class="container container--narrow page-section">
+      <div class="metabox metabox--position-up metabox--with-home-link">
+        <p>
+          <a class="metabox__blog-home-link" href="#"><i class="fa fa-home" aria-hidden="true"></i> Back to About Us</a> <span class="metabox__main">Our History</span>
+        </p>
+      </div>
+
+      <!-- <div class="page-links">
+        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+        <ul class="min-list">
+          <li class="current_page_item"><a href="#">Our History</a></li>
+          <li><a href="#">Our Goals</a></li>
+        </ul>
+      </div> -->
+
+      <div class="generic-content">
+      <?= the_content(); ?>  
+      </div>
+    </div>
 <?php
-    the_content();
+    endwhile;
     get_footer();
 ?>
