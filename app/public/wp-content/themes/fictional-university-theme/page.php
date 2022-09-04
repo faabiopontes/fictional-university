1976becf16de04b48428a0379d6573ea1523aa2f
Fabio Pontes <faabiopontes@gmail.com>
2022-09-04 20:43:13 -0300

    S5L18: Menu of Child Page Links

--- app/public/wp-content/themes/fictional-university-theme/page.php
+++ app/public/wp-content/themes/fictional-university-theme/page.php
@@ -17,22 +17,35 @@ while (have_posts()) :
   <div class="container container--narrow page-section">
     <?php
     $theParentID = wp_get_post_parent_id(get_the_ID());
-    if ($theParentID !== 0) : ?>
+    $isChild = $theParentID !== 0;
+    if ($isChild) : ?>
       <div class="metabox metabox--position-up metabox--with-home-link">
         <p>
           <a class="metabox__blog-home-link" href="<?= get_permalink($theParentID); ?>">
             <i class="fa fa-home" aria-hidden="true"></i> Back to <?= get_the_title($theParentID); ?></a> <span class="metabox__main"><?= the_title(); ?></span>
         </p>
       </div>
-    <?php endif; ?>
+    <?php
+    endif;
 
-    <!-- <div class="page-links">
-        <h2 class="page-links__title"><a href="#">About Us</a></h2>
+    $hasChildren = get_pages([
+      'child_of' => get_the_ID()
+    ]);
+
+    if ($isChild or $hasChildren) : ?>
+      <div class="page-links">
+        <h2 class="page-links__title"><a href="<?= get_permalink($theParentID); ?>"><?= get_the_title($theParentID); ?></a></h2>
         <ul class="min-list">
-          <li class="current_page_item"><a href="#">Our History</a></li>
-          <li><a href="#">Our Goals</a></li>
+          <?php
+          wp_list_pages([
+            'title_li' => NULL,
+            'child_of' => $isChild ? $theParentID : get_the_ID(),
+            'sort_column' => 'menu_order'
+          ]);
+          ?>
         </ul>
-      </div> -->
+      </div>
+    <?php endif; ?>
 
     <div class="generic-content">
       <?= the_content(); ?>
