b5af914a2b98f78868128288f454ac5b138eb954
Fabio Pontes <faabiopontes@gmail.com>
2022-09-22 20:29:22 -0300

    S7L29: Displaying Custom Post Types

--- app/public/wp-content/mu-plugins/university-post-types.php
+++ app/public/wp-content/mu-plugins/university-post-types.php
@@ -3,13 +3,17 @@
 function university_post_types()
 {
     register_post_type('event', [
+        'rewrite' => [
+            'slug' => 'events',
+        ],
+        'has_archive' => true,
         'public' => true,
         'labels' => [
             'name' => 'Events',
             'add_new_item' => 'Add New Event',
             'edit_item' => 'Edit Event',
             'all_items' => 'All Events',
-            'singular_name' => 'Event'
+            'singular_name' => 'Event',
         ],
         'menu_icon' => 'dashicons-calendar',
     ]);
