21bc256cc1b7dffc0adeb49a81466f4b62998621
Fabio Pontes <faabiopontes@gmail.com>
2022-09-29 16:26:12 -0300

    S7L31: Custom Fields

--- app/public/wp-content/themes/fictional-university-theme/front-page.php
+++ app/public/wp-content/themes/fictional-university-theme/front-page.php
@@ -22,11 +22,12 @@
 
             while ($homepageEvents->have_posts()) :
                 $homepageEvents->the_post();
+                $eventDate = new DateTime(get_field('event_date'));
             ?>
                 <div class="event-summary">
                     <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
-                        <span class="event-summary__month"><?php the_time('M'); ?></span>
-                        <span class="event-summary__day"><?php the_time('d'); ?></span>
+                        <span class="event-summary__month"><?= $eventDate->format('M'); ?></span>
+                        <span class="event-summary__day"><?= $eventDate->format('d'); ?></span>
                     </a>
                     <div class="event-summary__content">
                         <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
