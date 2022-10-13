9d57b5e03421055edfe3ba2395c5f95bbe2b2fa3
Fabio Pontes <faabiopontes@gmail.com>
2022-10-13 19:32:36 -0300

    S8L37: Displaying Relationships (Front-End)

--- app/public/wp-content/themes/fictional-university-theme/single-event.php
+++ app/public/wp-content/themes/fictional-university-theme/single-event.php
@@ -23,6 +23,23 @@ the_post(); // each time the_post runs it gets info from the next post
     <div class="generic-content">
         <?php the_content(); ?>
     </div>
+    <?php
+    $relatedPrograms = get_field('related_programs');
+    if ($relatedPrograms) :
+    ?>
+
+        <hr class="section-break">
+        <h2 class="headline headline--medium">Related Program(s)</h2>
+        <ul class="link-list min-list">
+            <?php
+            foreach ($relatedPrograms as $program) :
+            ?>
+                <li><a href="<?= get_the_permalink($program); ?>"><?= get_the_title($program); ?>
+                    <?php
+                endforeach;
+                    ?>
+        </ul>
+    <?php endif; ?>
 </div>
 <?php
 get_footer();
--- app/public/wp-content/themes/fictional-university-theme/single-program.php
+++ app/public/wp-content/themes/fictional-university-theme/single-program.php
@@ -23,6 +23,47 @@ the_post(); // each time the_post runs it gets info from the next post
     <div class="generic-content">
         <?php the_content(); ?>
     </div>
+    <?php
+    $today = date('Ymd');
+    $homepageEvents = new WP_Query([
+        'posts_per_page' => 2,
+        'post_type' => 'event',
+        'orderby' => 'meta_value',
+        'order' => 'asc',
+        'meta_key' => 'event_date',
+        'meta_query' => [
+            [
+                'key' => 'event_date',
+                'compare' => '>=',
+                'value' => $today,
+            ],
+            [
+                'key' => 'related_programs',
+                'compare' => 'LIKE',
+                'value' => '"' . get_the_ID() . '"',
+            ],
+        ]
+    ]);
+
+    while ($homepageEvents->have_posts()) :
+        $homepageEvents->the_post();
+        $eventDate = new DateTime(get_field('event_date'));
+    ?>
+        <div class="event-summary">
+            <a class="event-summary__date t-center" href="<?php the_permalink(); ?>">
+                <span class="event-summary__month"><?= $eventDate->format('M'); ?></span>
+                <span class="event-summary__day"><?= $eventDate->format('d'); ?></span>
+            </a>
+            <div class="event-summary__content">
+                <h5 class="event-summary__title headline headline--tiny"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+                <p><?= has_excerpt() ? get_the_excerpt() : wp_trim_words(get_the_content(), 18); ?> <a href="<?php the_permalink(); ?>" class="nu gray">Read more</a></p>
+            </div>
+        </div>
+
+    <?php
+    endwhile;
+    wp_reset_postdata(); // reset post to current URL base post 
+    ?>
 </div>
 <?php
 get_footer();
