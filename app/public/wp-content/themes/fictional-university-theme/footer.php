1da87d3da5c1eb13d49204bb116018539014ee3e
Fabio Pontes <faabiopontes@gmail.com>
2022-11-30 09:21:05 -0300

    S13L59: Preventing the Text Field Suggestions Pop-Up

--- app/public/wp-content/themes/fictional-university-theme/footer.php
+++ app/public/wp-content/themes/fictional-university-theme/footer.php
@@ -57,7 +57,7 @@
     <div class="search-overlay__top">
         <div class="container">
             <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
-            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term">
+            <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term" autocomplete="off">
             <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
         </div>
     </div>
