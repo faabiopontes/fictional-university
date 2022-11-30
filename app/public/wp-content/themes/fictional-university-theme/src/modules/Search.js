5eb8735518af79501592663220ebf306480e75f6
Fabio Pontes <faabiopontes@gmail.com>
2022-11-30 10:21:56 -0300

    S13L60: Keyboard Events in JavaScript

--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -1,29 +1,42 @@
 class Search {
   // 1. Describe and create/initiate our object
   constructor() {
-    this.openButton = document.querySelector(".search-trigger.js-search-trigger");
+    this.openButton = document.querySelector(
+      ".search-trigger.js-search-trigger"
+    );
     this.closeButton = document.querySelector(".search-overlay__close");
     this.searchOverlay = document.querySelector(".search-overlay");
     this.events();
+    this.isOverlayOpen = false;
   }
 
   // 2. Events
   events() {
-    console.log("events");
-    debugger;
-    this.openButton.addEventListener('click', this.openOverlay.bind(this));
-    this.closeButton.addEventListener('click', this.closeOverlay.bind(this));
+    this.openButton.addEventListener("click", this.openOverlay.bind(this));
+    this.closeButton.addEventListener("click", this.closeOverlay.bind(this));
+    document.addEventListener("keydown", this.keyPressDispatcher.bind(this));
   }
 
   // 3. Methods
   openOverlay() {
-    console.log("openOverlay");
     this.searchOverlay.classList.add("search-overlay--active");
+    document.body.classList.add("body-no-scroll");
+    this.isOverlayOpen = true;
   }
 
   closeOverlay() {
-    console.log("closeOverlay");
     this.searchOverlay.classList.remove("search-overlay--active");
+    document.body.classList.remove("body-no-scroll");
+    this.isOverlayOpen = false;
+  }
+
+  keyPressDispatcher(event) {
+    if (event.code === "KeyS" && !this.isOverlayOpen) {
+      this.openOverlay();
+    }
+    if (event.code === "Escape" && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
   }
 }
 
