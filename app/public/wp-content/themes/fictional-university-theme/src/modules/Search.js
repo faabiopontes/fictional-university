7dbc9b0494470b53f5b8eeaed8715c96431fa128
Fabio Pontes <faabiopontes@gmail.com>
2022-12-11 20:44:51 -0300

    fix: Removed console.log for input value

--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -28,7 +28,6 @@ class Search {
   typingLogic(event) {
     clearInterval(this.typingTimer);
     const inputValue = event.target.value;
-    console.log({ inputValue });
 
     if (inputValue === this.previousValue) {
       return;
