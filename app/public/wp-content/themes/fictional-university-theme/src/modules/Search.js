faf3c7e9a64a6cb67164a15d4b08684973d46599
Fabio Pontes <faabiopontes@gmail.com>
2022-12-11 16:54:42 -0300

    S13L61: Managing Time in JavaScript

--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -1,13 +1,14 @@
 class Search {
   // 1. Describe and create/initiate our object
   constructor() {
-    this.openButton = document.querySelector(
-      ".search-trigger.js-search-trigger"
-    );
+    this.openButton = document.querySelector(".js-search-trigger");
     this.closeButton = document.querySelector(".search-overlay__close");
     this.searchOverlay = document.querySelector(".search-overlay");
-    this.events();
+    this.searchField = document.querySelector("#search-term");
+    this.typingTimer;
     this.isOverlayOpen = false;
+    
+    this.events();
   }
 
   // 2. Events
@@ -15,9 +16,21 @@ class Search {
     this.openButton.addEventListener("click", this.openOverlay.bind(this));
     this.closeButton.addEventListener("click", this.closeOverlay.bind(this));
     document.addEventListener("keydown", this.keyPressDispatcher.bind(this));
+    this.searchField.addEventListener("keydown", this.typingLogic.bind(this));
   }
 
   // 3. Methods
+  typingLogic(event) {
+    clearInterval(this.typingTimer);
+
+    this.typingTimer = setTimeout(() => {
+      console.log("doSearch");
+    }, 500);
+
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
   openOverlay() {
     this.searchOverlay.classList.add("search-overlay--active");
     document.body.classList.add("body-no-scroll");
