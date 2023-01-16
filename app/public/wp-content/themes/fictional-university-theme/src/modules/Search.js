7dbd7e68ddb67c649a81b6530c9bcd224c88a23e
Fabio Pontes <faabiopontes@gmail.com>
2023-01-16 16:48:18 -0300

    S14L69: Quick Misc Edits

--- app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
+++ app/public/wp-content/themes/fictional-university-theme/src/modules/Search.js
@@ -1,6 +1,7 @@
 class Search {
   // 1. Describe and create/initiate our object
   constructor() {
+    this.addSearchHTML();
     this.resultsDiv = document.getElementById("search-overlay__results");
     this.openButtons = document.querySelectorAll(".js-search-trigger");
     this.closeButton = document.querySelector(".search-overlay__close");
@@ -16,6 +17,7 @@ class Search {
 
   // 2. Events
   events() {
+    this.addSearchHTML();
     this.openButtons.forEach((button) =>
       button.addEventListener("click", this.openOverlay.bind(this))
     );
@@ -53,7 +55,7 @@ class Search {
         <h2 class="search-overlay__section-title">General Information</h2>
         ${results}
       `;
-    }, 1500);
+    }, 750);
 
     this.previousValue = inputValue;
 
@@ -86,6 +88,8 @@ class Search {
   openOverlay() {
     this.searchOverlay.classList.add("search-overlay--active");
     document.body.classList.add("body-no-scroll");
+    this.searchField.value = "";
+    setTimeout(() => this.searchField.focus(), 301);
     this.isOverlayOpen = true;
   }
 
@@ -102,6 +106,26 @@ class Search {
     if (event.code === "Escape" && this.isOverlayOpen) {
       this.closeOverlay();
     }
+  }
+
+  addSearchHTML() {
+    document.body.insertAdjacentHTML(
+      "beforeend",
+      `
+      <div class="search-overlay">
+          <div class="search-overlay__top">
+              <div class="container">
+                  <i class="fa fa-search search-overlay__icon" aria-hidden="true"></i>
+                  <input type="text" class="search-term" placeholder="What are you looking for?" id="search-term" autocomplete="off">
+                  <i class="fa fa-window-close search-overlay__close" aria-hidden="true"></i>
+              </div>
+          </div>
+          <div class="container">
+              <div id="search-overlay__results"></div>
+          </div>
+      </div>
+      `
+    );
   }
 }
 
